b4698e23ad2b83b0d9719f31e6b8434713232b85
Anyelo <anyeloert@gmail.com>
2020-07-25 23:39:12 -0500

    creacion variable de entorno para conectar a la base de datos de mlab

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB = ''
 
 process.env.NODE_ENV === 'dev' ?  
     urlDB = 'mongodb://localhost:27017/cafe' : 
-    urlDB ='mongodb+srv://anyeloert:123456abc@cluster0.yykb4.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB =process.env.NODE_ENV.Mlab_Connect
 
 
 process.env.urlDB = urlDB
